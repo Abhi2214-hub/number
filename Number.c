87bd441bd847cf210ecb328c6a11995d5fbc0e28
Abhi2214-hub <68007150+Abhi2214-hub@users.noreply.github.com>
2020-07-12 22:16:14 +0530

    Created number.c
    this file print the default case if number is entered above 1

---
+++ Number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter the number");
+scanf("%d, &a);
+switch(a)
+{
+ case 0:printf("Case A");
+ case 1:printf("Case B");
+ break;
+ default:printf("Default Case");
+} 
+ }
